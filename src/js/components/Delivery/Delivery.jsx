d6311147672f4bc8044e01eaac9f95ff3b3ddda4
ahsanch11 <ahsan.zia11@gmail.com>
2018-09-16 00:06:26 -0700

    removed linting errors & optimization done

--- src/js/components/Delivery/Delivery.jsx
+++ src/js/components/Delivery/Delivery.jsx
@@ -1,114 +1,123 @@
 import React, { Fragment } from "react";
+import Skeleton from "react-loading-skeleton";
+import { connect } from "react-redux";
+import { func, arrayOf, shape, string } from "prop-types";
+
 import HeroTitle from "../HeroTitle/HeroTitle";
 import TextField from "../TextField/TextField";
 import Button from "../Button/Button";
-import { connect } from "react-redux";
 import * as actions from "../../actions";
-import Skeleton from "react-loading-skeleton";
 
-class SkeletonLoading extends React.Component {
-  render() {
-    return (
-      <Fragment>
-        <div id="Delivery">
-          <HeroTitle name="Delivery" section="2" />
-        </div>
+const SkeletonLoading = () => (
+  <Fragment>
+    <div id="Delivery">
+      <HeroTitle name="Delivery" section="2" />
+    </div>
 
-        <div className="d-form">
-          <div
-            style={{
-              margin: "auto",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              marginTop: 120
-            }}
-          >
-            <Skeleton width={600} height={90} />
-          </div>
-          <div
-            style={{
-              margin: "auto",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              marginTop: 120
-            }}
-          >
-            <Skeleton width={600} height={90} />
-          </div>
-          <div
-            style={{
-              margin: "auto",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              marginTop: 120
-            }}
-          >
-            <Skeleton width={600} height={90} />
-          </div>
-          <div
-            style={{
-              margin: "auto",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              marginTop: 120
-            }}
-          >
-            <Skeleton width={600} height={90} />
-          </div>
-          <div
-            style={{
-              margin: "auto",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              marginTop: 120
-            }}
-          >
-            <Skeleton width={600} height={90} />
-          </div>
-        </div>
-      </Fragment>
-    );
-  }
-}
+    <div className="d-form">
+      <div
+        style={{
+          margin: "auto",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 120
+        }}
+      >
+        <Skeleton width={600} height={90} />
+      </div>
+      <div
+        style={{
+          margin: "auto",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 120
+        }}
+      >
+        <Skeleton width={600} height={90} />
+      </div>
+      <div
+        style={{
+          margin: "auto",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 120
+        }}
+      >
+        <Skeleton width={600} height={90} />
+      </div>
+      <div
+        style={{
+          margin: "auto",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 120
+        }}
+      >
+        <Skeleton width={600} height={90} />
+      </div>
+      <div
+        style={{
+          margin: "auto",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 120
+        }}
+      >
+        <Skeleton width={600} height={90} />
+      </div>
+    </div>
+  </Fragment>
+);
 
 function showtextFields(Data) {
-  const textFields = Data.map(data => {
-    return (
-      <TextField
-        name={data.name}
-        type={data.type}
-        width={data.width}
-        key={Math.random()}
-      />
-    );
-  });
+  const textFields = Data.map(data => (
+    <TextField
+      name={data.name}
+      type={data.type}
+      width={data.width}
+      key={Math.random()}
+    />
+  ));
 
   return textFields;
 }
 class Delivery extends React.Component {
+  static propTypes = {
+    getDeliveryData: func.isRequired,
+    deliveryData: arrayOf(
+      shape({
+        name: string.isRequired,
+        width: string.isRequired,
+        type: string.isRequired
+      })
+    ).isRequired
+  };
+
   componentDidMount() {
     this.props.getDeliveryData();
   }
   render() {
-    return this.props.deliveryData.data.length
-      ? <Fragment>
-          <div id="Delivery">
-            <HeroTitle name="Delivery" section="2" />
+    return this.props.deliveryData.data.length ? (
+      <Fragment>
+        <div id="Delivery">
+          <HeroTitle name="Delivery" section="2" />
 
-            <div className="d-form">
-              <form>
-                {showtextFields(this.props.deliveryData.data)}
-                <Button value="Next step" arrow="true" />
-              </form>
-            </div>
+          <div className="d-form">
+            <form>
+              {showtextFields(this.props.deliveryData.data)}
+              <Button value="Next step" arrow="true" />
+            </form>
           </div>
-        </Fragment>
-      : <SkeletonLoading />;
+        </div>
+      </Fragment>
+    ) : (
+      <SkeletonLoading />
+    );
   }
 }
 
@@ -118,6 +127,9 @@ function mapStateToProps({ deliveryData }) {
   };
 }
 
-export default connect(mapStateToProps, {
-  getDeliveryData: actions.getDeliveryData
-})(Delivery);
+export default connect(
+  mapStateToProps,
+  {
+    getDeliveryData: actions.getDeliveryData
+  }
+)(Delivery);
