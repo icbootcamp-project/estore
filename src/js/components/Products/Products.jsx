059af4f646672a5f62a65397d7dda2e8e337baea
Taimoor Khan <taimoor.m.k@live.com>
2018-10-16 22:00:02 +0500

    minor adjustments and lint fixes

--- src/js/components/Products/Products.jsx
+++ src/js/components/Products/Products.jsx
@@ -1,44 +1,31 @@
-import React , {Component , Fragment} from "react";
+import React, { Component, Fragment } from "react";
 import { arrayOf, shape } from "prop-types";
 
 import Categories from "../Categories";
 import Filterproduct from "./FilterProduct";
 import Item from "./Item";
-import ProductItmes from "./data";
+import ProductItems from "./data";
 
 export default class Products extends Component {
 
-    static propTypes = {
-        categories: arrayOf(
-            shape()).isRequired
-    }
+  static propTypes = {
+    categories: arrayOf(shape({})).isRequired
+  }
 
-    constructor(){
-        super();
-        this.state = {};
+  renderItems = () => (
+    ProductItems.map((item) => <Item key={item.id} item={item} />)
+  )
 
-    }
-    static renderItems(){
-        return ProductItmes.map(
-            (item) => <Item item={item} />
-            
-        )
-    }
-
-    render(){
-        const { categories } = this.props;
-        return(
-          <Fragment>
-            <Categories categories={categories} />
-            <Filterproduct />
-            <div id="product-container">
-              {this.renderItems()}
-            </div>
-          </Fragment>
-            
-        )
-    }
-
-
-
+  render() {
+    const { categories } = this.props;
+    return (
+      <Fragment>
+        <Categories categories={categories} />
+        <Filterproduct />
+        <div id="product-container">
+          {this.renderItems()}
+        </div>
+      </Fragment>
+    )
+  }
 };
