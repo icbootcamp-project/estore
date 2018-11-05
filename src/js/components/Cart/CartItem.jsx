f9e07760822ff20da7f861555564084bd042f9de
FadiGitHub <34411433+FadiGitHub@users.noreply.github.com>
2018-11-05 18:17:46 +0500

    Fahad (#23)
    * price & counter
    
    * total amount with GST
    
    * total amount with GST
    
    * code cleaning
    
    * code cleaning

--- src/js/components/Cart/CartItem.jsx
+++ src/js/components/Cart/CartItem.jsx
@@ -1,59 +1,54 @@
 // ########## Import Dependencies Here ##########
-import React, { Fragment } from "react";
-import { shape, func } from "prop-types";
+import React, { Fragment } from 'react';
+import { shape, func } from 'prop-types';
 
 // ########## Import Components Here ##########
-import Counter from "./Counter";
+import Counter from './Counter';
 
 const CartItem = props => {
-  const { cartItem, addCounter, lessCounter } = props;
-  return (
-    <Fragment>
-      <div className="cart-item">
-        <div className="img">
-          <div className={`item-frame-${cartItem.cartItemId.slice(5)}`}>
+	const { cartItem, addCounter, lessCounter } = props;
+	return (
+		<Fragment>
+			<div className='cart-item'>
+				<div className='img'>
+					<div className={`item-frame-${cartItem.cartItemId.slice(5)}`}>
+						<div
+							style={{
+								background     : `url(${cartItem.cartItemImage})`,
+								noRepeat       : 'center',
+								width          : '12em',
+								height         : '12em',
+								backgroundSize : 'cover',
+								overflow       : 'hidden',
+								borderRadius   : '1em'
+							}}
+						/>
+					</div>
+				</div>
 
-            <div style={{
-              background: `url(${cartItem.cartItemImage})`, "noRepeat": "center", width: "12em",
-              height: "12em",
-              backgroundSize: "cover",
-              overflow: "hidden",
-              borderRadius: "1em"
+				<div className='detail'>
+					<h3 className='item-name'>{cartItem.cartItemName}</h3>
+					<p className='item-size'>{cartItem.cartItemSize} </p>
+					<div className='co'>
+						<div id='counter'>
+							<Counter addCounter={addCounter} lessCounter={lessCounter} cartItem={cartItem} />
+						</div>
+						<div className='del-icon' />
+					</div>
+				</div>
 
-            }}
-            />
-
-
-          </div>
-        </div>
-
-        <div className="detail">
-          <h3 className="item-name">{cartItem.cartItemName}</h3>
-          <p className="item-size">{cartItem.cartItemSize} </p>
-          <div className="co">
-            <div id="counter">
-              <Counter
-                addCounter={addCounter}
-                lessCounter={lessCounter}
-                cartItem={cartItem}
-              />
-            </div>
-            <div className="del-icon" />
-          </div>
-        </div>
-
-        <div className="sec-amt">
-          <div className="amount">Rs: {cartItem.cartItemDisplayPrice}</div>
-        </div>
-      </div>
-    </Fragment >
-  );
+				<div className='sec-amt'>
+					<div className='amount'>Rs: {cartItem.cartItemValue}</div>
+				</div>
+			</div>
+		</Fragment>
+	);
 };
 
 CartItem.propTypes = {
-  cartItem: shape().isRequired,
-  addCounter: func.isRequired,
-  lessCounter: func.isRequired
+	cartItem    : shape().isRequired,
+	addCounter  : func.isRequired,
+	lessCounter : func.isRequired
 };
 
 export default CartItem;
--- src/js/sagas/index.js
+++ src/js/sagas/index.js
@@ -1,53 +1,46 @@
 // ########## Import Dependencies Here ##########
-import {
-  takeLatest,
-  fork,
-  all
-} from "redux-saga/effects";
+import { takeLatest, fork, all } from 'redux-saga/effects';
 
 // ########## Import Components Here ##########
-import * as types from "../constants";
-import loadHeaderSaga from "./loadHeaderSaga";
-import loadCategoriesSaga from "./loadCategoriesSaga";
-import loadSubCategoriesGallerySaga from "./loadSubCategoriesGallerySaga";
-import loadFooterSaga from "./loadFooterSaga";
-import getDeliveryDataSaga from "./getDeliveryDataSaga";
-import loadCartSaga from "./loadCartSaga";
+import * as types from '../constants';
+import loadHeaderSaga from './loadHeaderSaga';
+import loadCategoriesSaga from './loadCategoriesSaga';
+import loadSubCategoriesGallerySaga from './loadSubCategoriesGallerySaga';
+import loadFooterSaga from './loadFooterSaga';
+import getDeliveryDataSaga from './getDeliveryDataSaga';
+import loadCartSaga from './loadCartSaga';
 
 function* watchLoadHeader() {
-  yield takeLatest(types.LOAD_HEADER, loadHeaderSaga);
+	yield takeLatest(types.LOAD_HEADER, loadHeaderSaga);
 }
 
 function* watchLoadCategories() {
-  yield takeLatest(types.LOAD_CATEGORIES, loadCategoriesSaga);
+	yield takeLatest(types.LOAD_CATEGORIES, loadCategoriesSaga);
 }
 
 function* watchLoadSubCategoriesGallery() {
-  yield takeLatest(
-    types.LOAD_SUB_CATEGORIES_GALLERY,
-    loadSubCategoriesGallerySaga
-  );
+	yield takeLatest(types.LOAD_SUB_CATEGORIES_GALLERY, loadSubCategoriesGallerySaga);
 }
 
 function* watchLoadFooter() {
-  yield takeLatest(types.LOAD_FOOTER, loadFooterSaga);
+	yield takeLatest(types.LOAD_FOOTER, loadFooterSaga);
 }
 
 function* watchGetDeliveryData() {
-  yield takeLatest(types.GET_DELIVERY_DATA, getDeliveryDataSaga);
+	yield takeLatest(types.GET_DELIVERY_DATA, getDeliveryDataSaga);
 }
 
 function* watchLoadCart() {
-  yield takeLatest(types.LOAD_CART, loadCartSaga);
+	yield takeLatest(types.LOAD_CART, loadCartSaga);
 }
 
 export default function* rootSaga() {
-  yield all([
-    fork(watchLoadHeader),
-    fork(watchLoadCategories),
-    fork(watchLoadSubCategoriesGallery),
-    fork(watchLoadFooter),
-    fork(watchLoadCart),
-    fork(watchGetDeliveryData)
-  ]);
+	yield all([
+		fork(watchLoadHeader),
+		fork(watchLoadCategories),
+		fork(watchLoadSubCategoriesGallery),
+		fork(watchLoadFooter),
+		fork(watchLoadCart),
+		fork(watchGetDeliveryData)
+	]);
 }
